0498ac0a1648566fbea888aca87244d245852030
mazdafil <guskov.artem86@gmail.com>
2017-03-30 23:13:31 +0700

    DB/Quest: Fix wrong quests from Marshal McBride

--- sql/update/world/2017_03_27_02_world_custom9.sql
+++ sql/update/world/2017_03_27_02_world_custom9.sql
@@ -1,1 +1,2 @@
-UPDATE `world`.`quest_template_addon` SET `PrevQuestID`='31139' WHERE  `ID`=28770;
+UPDATE `quest_template_addon` SET `PrevQuestID`='31139' WHERE  `ID`=28770;
+UPDATE `quest_template_addon` SET `PrevQuestID`='28770' WHERE  `ID`=28785;
