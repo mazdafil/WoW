a71cdc33cf8d5de340e50c962b21621bccb7bea7
mazdafil <guskov.artem86@gmail.com>
2017-04-04 15:30:23 +0700

    DB Add Russian localisation for gossip menu

--- sql/update/world/2017_03_27_02_world_custom15.sql
+++ sql/update/world/2017_03_27_02_world_custom15.sql
@@ -1,4 +1,3 @@
-SET NAMES cp1251;
 DELETE FROM `locales_gossip_menu_option` WHERE `menu_id`=401 AND `id`=0;
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc1`, `option_text_loc2`, `option_text_loc3`, `option_text_loc4`, `option_text_loc5`, `option_text_loc6`, `option_text_loc7`, `option_text_loc8`, `box_text_loc1`, `box_text_loc2`, `box_text_loc3`, `box_text_loc4`, `box_text_loc5`, `box_text_loc6`, `box_text_loc7`, `box_text_loc8`) VALUES (401, 0, NULL, NULL, 'Druide', NULL, NULL, NULL, NULL, 'Äðóèä', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 DELETE FROM `locales_gossip_menu_option` WHERE `menu_id`=401 AND `id`=1;
