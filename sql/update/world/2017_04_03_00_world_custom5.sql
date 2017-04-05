cec3e28d23a0f0fc54660670856357f4fc49d85d
mazdafil <guskov.artem86@gmail.com>
2017-04-05 23:05:35 +0700

    Fix quest 26214

---
+++ sql/update/world/2017_04_03_00_world_custom5.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_loot_template` WHERE entry = 515 AND item = 57756;
+INSERT INTO `creature_loot_template` VALUES (515, 57756, 0, 22, 1, 1, 0, 1, 1, NULL);
+DELETE FROM `creature_loot_template` WHERE entry = 126 AND item = 57756;
+INSERT INTO `creature_loot_template` VALUES (126, 57756, 0, 20, 1, 1, 0, 1, 1, NULL);
