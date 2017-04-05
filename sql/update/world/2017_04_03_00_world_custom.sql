8cd164d190099128582dbe9e5b5b6b828ce3b962
mazdafil <guskov.artem86@gmail.com>
2017-04-05 22:03:41 +0700

    Fix quest 26209

---
+++ sql/update/world/2017_04_03_00_world_custom.sql
@@ -0,0 +1,2 @@
+UPDATE `locales_gossip_menu_option` SET `option_text_loc8`='Вы не видели кто убил Хмуробровов?' WHERE  `menu_id`=11635 AND `id`=0;
+UPDATE `locales_gossip_menu_option` SET `option_text_loc8`='Может быть пара медных монет развяжет Вам язык. Теперь скажите мне, Вы не видели кто убил Хмуробровов?' WHERE  `menu_id`=11635 AND `id`=1;
