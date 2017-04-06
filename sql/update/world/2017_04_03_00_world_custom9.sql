72c63a96af4843adb23c7a5f8fde8db72cca5ae1
mazdafil <guskov.artem86@gmail.com>
2017-04-06 16:55:35 +0700

    Fix npc 42405

---
+++ sql/update/world/2017_04_03_00_world_custom9.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_area` WHERE spell = 79229;
+INSERT INTO `spell_area` VALUES (79229, 40, 26215, 26232, 0, 0, 2, 1, 74, 9);
+UPDATE `creature_template_addon` SET auras = '78284' WHERE entry = 42405;
