4be968386bafbdd3e32c9a087066355e2cf83f04
mazdafil <guskov.artem86@gmail.com>
2017-04-09 00:52:20 +0700

    Fix Westfail

---
+++ sql/update/world/2017_04_03_00_world_custom12.sql
@@ -0,0 +1,14 @@
+DELETE FROM `quest_template_addon` WHERE ID = 26286;
+INSERT INTO `quest_template_addon` (`ID`, `NextQuestID`) VALUES (26286, 26289);
+UPDATE `quest_template_addon` SET NextQuestID = 26292 WHERE ID = 26291;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=42407;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=124;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=449;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=589;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=594;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=452;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=501;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=54371;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=54372;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=54373;
+UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=234;
