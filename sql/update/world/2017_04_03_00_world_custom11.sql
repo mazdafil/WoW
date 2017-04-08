241629ac9f40cf4128ce3446185f847ccf5040c1
mazdafil <guskov.artem86@gmail.com>
2017-04-08 23:52:19 +0700

    Fix questchain 26270

--- sql/update/world/2017_04_03_00_world_custom11.sql
+++ sql/update/world/2017_04_03_00_world_custom11.sql
@@ -1,2 +1,3 @@
+DELETE FROM `quest_template_addon` WHERE ID = 26237 OR ID = 26266;
 INSERT INTO `quest_template_addon` (`ID`, `NextQuestID`, ExclusiveGroup) VALUES (26237, 26270, -26237);
-INSERT INTO `quest_template_addon` (`ID`, `NextQuestID`, ExclusiveGroup) VALUES (26270, 26266, -26237);
+INSERT INTO `quest_template_addon` (`ID`, `NextQuestID`, ExclusiveGroup) VALUES (26266, 26270, -26237);
