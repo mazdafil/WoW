8dd51461faf92fe55c728842fd4a2dd5449b868f
mazdafil <guskov.artem86@gmail.com>
2017-04-12 00:18:46 +0700

    Fix spell 205228

--- sql/update/world/2017_04_11_world.sql
+++ sql/update/world/2017_04_11_world.sql
@@ -1,2 +1,2 @@
 UPDATE `spell_script_names` SET ScriptName = 'spell_pal_templar_verdict' WHERE spell_id = 85256;
-INSERT INTO `spell_script_names` VALUES (205228, 'pell_pal_consecration');
+INSERT INTO `spell_script_names` VALUES (205228, 'spell_pal_consecration');
