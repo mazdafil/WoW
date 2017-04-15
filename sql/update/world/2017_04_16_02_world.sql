52dce5e99be43d78b1af290b749ce6076ad2bc7b
mazdafil <guskov.artem86@gmail.com>
2017-04-16 02:36:07 +0700

    Fix population Deadmines

---
+++ sql/update/world/2017_04_16_02_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `creature` WHERE (id=46890 OR id =46903 OR id=46902 OR id=46906 OR id=46889 ) AND (map = 36);
+DELETE FROM `creature` WHERE (id=50595 OR id =46612 OR id=24935 OR id=46614 OR id=46613 ) AND (map = 36); 
