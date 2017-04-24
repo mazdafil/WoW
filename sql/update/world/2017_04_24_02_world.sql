1b36dd47dbafcd71795ee4764bd0d3968ea14638
mazdafil <guskov.artem86@gmail.com>
2017-04-24 15:38:39 +0700

    Add scripting Stormwind

---
+++ sql/update/world/2017_04_24_02_world.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_archmage_malin_2708' WHERE  entry = 2708;
+INSERT INTO `areatrigger_scripts` VALUES (2746, 'at_stormwind_command_board');
+INSERT INTO `areatrigger_scripts` VALUES (7350, 'at_stormwind_teleport_area');
--- src/server/scripts/EasternKingdoms/zone_stormwind_city.cpp
+++ src/server/scripts/EasternKingdoms/zone_stormwind_city.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2016 ArkCORE <http://www.arkania.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,6 +16,101 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "script_helper.h"
+#include "Creature.h"
+#include "GameObjectAI.h"
+#include "GameObject.h"
+#include "ObjectAccessor.h"
+#include "ObjectMgr.h"
+#include "Player.h"
+#include "ScriptedCreature.h"
+#include "ScriptedEscortAI.h"
+#include "ScriptMgr.h"
+#include "SpellMgr.h"
+#include "Transport.h"
+#include "TransportMgr.h"
+#include "Vehicle.h"
+
+enum eStormwind
+{
+    QUEST_A_PERSONAL_SUMMONS = 28825,
+};
+
+ // 2708
+class npc_archmage_malin_2708 : public CreatureScript
+{
+public:
+    npc_archmage_malin_2708() : CreatureScript("npc_archmage_malin_2708") { }
+
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action) override
+    {
+        player->PlayerTalkClass->ClearMenus();
+        if (action == 1)
+        {
+            player->CLOSE_GOSSIP_MENU();
+            player->TeleportTo(1, -3752.51f, -4456.33f, 64.97f, 0.7286f);
+            return true;
+        }
+        return false;
+    }
+};
+
+// 2746
+class at_stormwind_command_board : public AreaTriggerScript
+{
+public:
+    at_stormwind_command_board() : AreaTriggerScript("at_stormwind_command_board") { }
+
+    bool OnTrigger(Player* player, const AreaTriggerEntry* at, bool /*entered*/) override
+    {
+        if (player->getLevel() < 80 || player->GetTeamId() != TEAM_ALLIANCE)
+            return false;
+
+        if (player->GetQuestStatus(28825) != QUEST_STATUS_NONE)
+            return false;
+
+        if (Quest const* quest = sObjectMgr->GetQuestTemplate(QUEST_A_PERSONAL_SUMMONS))
+            if (GameObject* go = player->FindNearestGameObject(206111, 50.0f))
+                if (player->CanTakeQuest(quest, false))
+                {
+                    player->AddQuest(quest, go);
+                    return true;
+                }
+
+        return false;
+    }
+};
+
+// 7350
+class at_stormwind_teleport_area : public AreaTriggerScript
+{
+public:
+    at_stormwind_teleport_area() : AreaTriggerScript("at_stormwind_teleport_area") { }
+
+    bool OnTrigger(Player* player, const AreaTriggerEntry* at, bool /*entered*/) override
+    {
+        if (player->getLevel() < 80 || player->GetTeamId() != TEAM_ALLIANCE)
+            return false;
+
+        if (player->GetQuestStatus(28825) != QUEST_STATUS_NONE)
+            return false;
+
+        if (Quest const* quest = sObjectMgr->GetQuestTemplate(QUEST_A_PERSONAL_SUMMONS))
+            if (Creature* npc = player->FindNearestCreature(45226, 50.0f))
+                if (player->CanTakeQuest(quest, false))
+                {
+                    player->AddQuest(quest, npc);
+                    return true;
+                }
+
+        return false;
+    }
+};
+
+
 void AddSC_stormwind_city()
 {
+    new npc_archmage_malin_2708();
+    new at_stormwind_command_board();
+    new at_stormwind_teleport_area();
 }
