d40d22dc232df80a0ed7fd68fe4019fb388589f1
mazdafil <guskov.artem86@gmail.com>
2017-05-06 03:32:16 +0700

    Randolph Moloch

---
+++ sql/update/world/2017_05_06_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE entryorguid = 46383 AND event_type = 61;
+DELETE FROM `smart_scripts` WHERE entryorguid = 46383 AND event_param2 = 30;
+DELETE FROM `smart_scripts` WHERE entryorguid = 46383 AND event_param2 = 50;
