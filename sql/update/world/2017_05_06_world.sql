cb96f8f0fb153cdbaaa4b4ad3d2a53ffe40e5b6b
mazdafil <guskov.artem86@gmail.com>
2017-05-07 23:52:22 +0700

    Fix crush in the Lakeshire

--- sql/update/world/2017_05_06_world.sql
+++ sql/update/world/2017_05_06_world.sql
@@ -1,3 +1,4 @@
 DELETE FROM `smart_scripts` WHERE entryorguid = 46383 AND event_type = 61;
 DELETE FROM `smart_scripts` WHERE entryorguid = 46383 AND event_param2 = 30;
 DELETE FROM `smart_scripts` WHERE entryorguid = 46383 AND event_param2 = 50;
+UPDATE `creature_template` SET unit_flags = 512 WHERE entry IN (42492, 42515);
---
+++ sql/update/world/2017_05_07_world.sql
@@ -0,0 +1,3 @@
+INSERT INTO `creature_equip_template` (CreatureID, ID, VerifiedBuild) VALUES (43248, 1, 18019); 
+INSERT INTO `creature_equip_template` (CreatureID, ID, VerifiedBuild) VALUES (43249, 1, 18019); 
+UPDATE `creature` SET equipment_id = 1 WHERE guid IN (334776, 334772);
