5ab684c4b57aa307071721c6bcc9e86e1e72ab20
mazdafil <guskov.artem86@gmail.com>
2017-05-08 03:49:49 +0700

    Fix quests npc 43221

--- sql/update/world/2017_05_08_2_world.sql
+++ sql/update/world/2017_05_08_2_world.sql
@@ -1,2 +1,3 @@
 UPDATE `quest_template_addon` SET NextQuestID = 26571 WHERE ID = 26568;
+UPDATE `quest_template_addon` SET NextQuestID = 26567 WHERE ID = 26545;
 INSERT INTO `quest_template_addon` (ID, PrevQuestID, NextQuestID) VALUES (26571, 26568, 26586);
